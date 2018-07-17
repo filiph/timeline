a84ddcf68e624078a04f600e968f76ef026eba8c
Filip Hracek <filip.hracek@gmail.com>
2018-07-17 13:49:19 -0700

    Upgrade to AngularDart 5

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,9 @@
 // Copyright (c) 2017, filiph. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/platform/browser.dart';
-
-import 'package:timeline/app_component.dart';
+import 'package:angular/angular.dart';
+import 'package:timeline/app_component.template.dart' as ng;
 
 void main() {
-  bootstrap(AppComponent);
+  runApp(ng.AppComponentNgFactory);
 }
